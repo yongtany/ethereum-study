ded37494a451f6f9161630ca692229370022c925
yongtany <yongn1494@gmail.com>
2019-04-15 19:56:45 +0900

    Testing Function Modifiers

--- lottery/test/lottery.test.js
+++ lottery/test/lottery.test.js
@@ -72,4 +72,15 @@ describe('Lottery Contract', () => {
       assert(err);
     }
   });
+
+  it('only manager can call pickWinner', async () => {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[1]
+      });
+      assert(false);
+    } catch(err) {
+      assert(err);
+    }
+  })
 });
