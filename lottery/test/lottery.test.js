66dc4fd384405f6d8f46e3c06be84f60fba2d7f3
yongtany <yongn1494@gmail.com>
2019-04-15 19:44:45 +0900

    Asserting Multiple Players

--- lottery/test/lottery.test.js
+++ lottery/test/lottery.test.js
@@ -31,7 +31,33 @@ describe('Lottery Contract', () => {
       from: accounts[0]
     });
 
-    assert.equal(accounts[0], players[0])
+    assert.equal(accounts[0], players[0]);
     assert.equal(1, players.length)
+  });
+
+  it('allows multiple account to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(3, players.length)
   });
 });
