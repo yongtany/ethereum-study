5c9de3c86f44d3b6c12bc11b1bcec67b78ea1984
yongtany <yongn1494@gmail.com>
2019-04-15 19:52:13 +0900

    Try-Catch Assertions

--- lottery/test/lottery.test.js
+++ lottery/test/lottery.test.js
@@ -60,4 +60,16 @@ describe('Lottery Contract', () => {
     assert.equal(accounts[2], players[2]);
     assert.equal(3, players.length)
   });
+
+  it('requires a minumum amount of ether to enter', async ()=> {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 0
+      });
+      assert(false);
+    } catch(err) {
+      assert(err);
+    }
+  });
 });
