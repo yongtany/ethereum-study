d41bb4ff11c4856c4812a0e1b51326180ba29eca
yongtany <yongn1494@gmail.com>
2019-04-17 19:51:46 +0900

    Rendering Contract Data

--- lottery-client/src/App.js
+++ lottery-client/src/App.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import web3 from './web3';
+import lottery from './lottery';
 
 class App extends Component {
-  render() {
+  state = {
+    manager: ''
+  }
 
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+
+    this.setState({ manager });
+  }
+
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by {this.state.manager}.</p>
       </div>
     );
   }
