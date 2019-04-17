8a149b7a2aa4925330916706e228b02cb8f15050
yongtany <yongn1494@gmail.com>
2019-04-17 12:42:12 +0900

    Local Contract Instances

--- lottery-client/src/App.js
+++ lottery-client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
