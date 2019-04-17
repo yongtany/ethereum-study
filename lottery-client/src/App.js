c83681746203f5ad16ebfd3350173c78038b162c
yongtany <yongn1494@gmail.com>
2019-04-17 20:11:58 +0900

    Create Enter Form

--- lottery-client/src/App.js
+++ lottery-client/src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   state = {
     manager: '',
     players: [],
-    balance: ''
+    balance: '',
+    value: ''
   };
 
   async componentDidMount() {
@@ -17,6 +18,17 @@ class App extends Component {
     this.setState({ manager, players, balance });
   }
 
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether'),
+    });
+  };
+
   render() {
     return (
       <div>
@@ -26,6 +38,19 @@ class App extends Component {
           There are currently {this.state.players.length} people entered.
           competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
+
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
