8ed810c8feae51f17f3ee14d11ab7732f0b60b89
yongtany <yongn1494@gmail.com>
2019-05-07 20:49:36 +0900

    Refactoring deployment

--- crowdFund/ethereum/deploy.js
+++ crowdFund/ethereum/deploy.js
@@ -1,1 +1,28 @@
-s
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'example just crush sweet senior embrace episode unique kid kiss clever lumber',
+  'https://rinkeby.infura.io/v3/5f9376ca441a4d048e54d330a8a4d9b8'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
+
+
