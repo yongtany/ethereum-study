1a48cc741a5326586f3031bc6d0ee40628c9bb9c
yongtany <yongn1494@gmail.com>
2019-05-12 20:18:32 +0900

    Add a css

--- crowdFund/pages/index.js
+++ crowdFund/pages/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component {
@@ -22,7 +22,18 @@ class CampaignIndex extends Component {
   }
 
   render() {
-    return <div>{this.renderCampaigns()}</div>
+    return <div>
+      <link
+        rel="stylesheet"
+        href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+      />
+      {this.renderCampaigns()}
+      <Button
+        content="Create Campaign"
+        icon="add circle"
+        primary
+      />
+    </div>;
   }
 }
 
