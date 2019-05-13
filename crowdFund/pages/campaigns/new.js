9d6d379ee01ab2a04a5919ab1a087fee9a2fb075
yongtany <yongn1494@gmail.com>
2019-05-13 17:49:57 +0900

    Add Form and Submittal

--- crowdFund/pages/campaigns/new.js
+++ crowdFund/pages/campaigns/new.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import { Form, Button, Input } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
+  state = {
+    minimumContribution: ''
+  };
+
+  onSubmit = async event => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    await factory.methods
+      .createCampaign(this.state.minimumContribution)
+      .send({
+        from: accounts[0]
+      });
+  };
+
   render() {
     return (
       <Layout>
-        <h1>New Campaign!</h1>
+        <h1>Create a Project</h1>
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={event =>
+                this.setState({ minimumContribution: event.target.value })}
+            />
+          </Form.Field>
+          <Button primary>Create!</Button>
+        </Form>
       </Layout>
     );
   }
