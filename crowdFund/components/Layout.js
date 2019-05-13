1e83eceb0effb1dfe4e61381b05f7b276be393f9
yongtany <yongn1494@gmail.com>
2019-05-13 19:00:50 +0900

    Form error handling

--- crowdFund/components/Layout.js
+++ crowdFund/components/Layout.js
@@ -1,7 +1,7 @@
- import React from 'react';
- import { Container } from 'semantic-ui-react';
- import Head from 'next/head';
- import Header from './Header';
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import Head from 'next/head';
+import Header from './Header';
 
  export default props => {
   return (
--- crowdFund/pages/campaigns/new.js
+++ crowdFund/pages/campaigns/new.js
@@ -1,40 +1,62 @@
 import React, { Component } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
   state = {
-    minimumContribution: ''
+    minimumContribution: '',
+    name: '',
+    errorMessage: ''
   };
+
+  onChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
 
   onSubmit = async event => {
     event.preventDefault();
 
-    const accounts = await web3.eth.getAccounts();
-    await factory.methods
-      .createCampaign(this.state.minimumContribution)
-      .send({
-        from: accounts[0]
-      });
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+        .createCampaign(this.state.name, this.state.minimumContribution)
+        .send({
+          from: accounts[0]
+        });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
   };
 
   render() {
     return (
       <Layout>
         <h1>Create a Project</h1>
-        <Form>
+
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
+              placeholder="Wei"
               label="wei"
               labelPosition="right"
               value={this.state.minimumContribution}
-              onChange={event =>
-                this.setState({ minimumContribution: event.target.value })}
+              onChange={this.onChange}
+              name="minimumContribution"
+            />
+            <label>Title</label>
+            <Input
+              placeholder="Title"
+              value={this.state.name}
+              onChange={this.onChange}
+              name="name"
             />
           </Form.Field>
+          <Message error header="Oops!" content={this.state.errorMessage} />
           <Button primary>Create!</Button>
         </Form>
       </Layout>
