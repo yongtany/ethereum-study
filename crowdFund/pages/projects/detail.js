195f56daf3d68b8044de8c5c1af7cd4757eddc94
yongtany <yongn1494@gmail.com>
2019-05-19 20:48:07 +0900

    One card per property

--- crowdFund/pages/projects/detail.js
+++ crowdFund/pages/projects/detail.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
 import Project from '../../ethereum/project';
+import web3 from '../../ethereum/web3';
+
+import { Card } from 'semantic-ui-react';
 
 class ProjectDetail extends Component {
   static async getInitialProps(props) {
@@ -18,10 +21,57 @@ class ProjectDetail extends Component {
     };
   }
 
+  renderCards() {
+    const  {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount
+    } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of Manager',
+        description:
+          'The manager created this campaign and can create requests to withdraw money',
+        style: { overflowWrap: 'break-word' }
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum Contribution (wei)',
+        description:
+          'You must contribute at least this much wei to become an approver'
+      },
+      {
+        header: requestsCount,
+        meta: 'Number of Requests',
+        description:
+          'A request tries to withdraw money from the contract. Requests must be approved by approvers'
+      },
+      {
+        header: approversCount,
+        meta: 'Number of Approvers',
+        description:
+          'Number of people who have already donated to this campaign'
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign Balance (ether)',
+        description:
+          'The balance is how much money this campaign has left to spend.'
+      }
+    ];
+
+    return <Card.Group items={items} />;
+  }
+
   render() {
     return  (
       <Layout>
         <h3>Project Detail</h3>
+        {this.renderCards()}
       </Layout>
     );
   }
